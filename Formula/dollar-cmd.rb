8393b8e2aee939b79119756d368866137537a2a7
cffnpwr <cffnpwr@gmail.com>
2026-07-20 02:04:37 +0900

    feat: build formulae from source and add bottle bump workflow

--- Formula/dollar-cmd.rb
+++ Formula/dollar-cmd.rb
@@ -1,6 +1,8 @@
 class DollarCmd < Formula
   desc "Joke command that makes a pasted `$ cmd args` line just work"
   homepage "https://github.com/cffnpwr/dollar-cmd"
+  url "https://github.com/cffnpwr/dollar-cmd/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "dd3a43c6134217270a098c19be0fe377466069bd857089af1ec62a4b797e8a78"
   license "MIT"
 
   livecheck do
@@ -8,26 +10,11 @@ class DollarCmd < Formula
     strategy :github_latest
   end
 
-  on_macos do
-    on_arm do
-      url "https://github.com/cffnpwr/dollar-cmd/releases/download/v1.0.0/dollar-cmd_1.0.0_aarch64-apple-darwin.tar.gz"
-      sha256 "506a2e7fd685a1c0b62dd401cf7ebe56882fe53432cc84bf28e8a3e47273ad3a"
-    end
-  end
-
-  on_linux do
-    on_arm do
-      url "https://github.com/cffnpwr/dollar-cmd/releases/download/v1.0.0/dollar-cmd_1.0.0_aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d6e4bed7c0350e867cf754b4c95834fb4e150df66b5c9f9576ba3a72638b736c"
-    end
-    on_intel do
-      url "https://github.com/cffnpwr/dollar-cmd/releases/download/v1.0.0/dollar-cmd_1.0.0_x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2bb437f898cba23557db02b994cfe66f42b82eacbb1d6b50e78831027efd0257"
-    end
-  end
+  depends_on "rust" => :build
 
   def install
-    bin.install "$"
+    system "cargo", "install", *std_cargo_args
+    mv bin/"dollar-cmd", bin/"$"
   end
 
   test do
